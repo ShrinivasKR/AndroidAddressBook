047f957df8a1ac9268bbaf7b8289ad21362397ac
Shrinivas Ramanath <shrinivas.k.ramanath@gmail.com>
2016-09-12 13:05:02 -0700

    Added contact list.

--- app/src/main/java/com/example/shrinivasramanath/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/shrinivasramanath/myfirstapp/MainActivity.java
@@ -8,11 +8,15 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    ListView contactList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,16 +24,58 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        EditText usernameField = (EditText) findViewById(R.id.username);
-        EditText passwordField = (EditText) findViewById(R.id.password);
+        EditText searchField = (EditText) findViewById(R.id.searchField);
+//        EditText passwordField = (EditText) findViewById(R.id.password);
 
-        Button loginButton = (Button)this.findViewById(R.id.login);
-        loginButton.setOnClickListener(new View.OnClickListener() {
+//        Button loginButton = (Button)this.findViewById(R.id.login);
+//        loginButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                finish();
+//            }
+//        });
+
+        // Get ListView object from xml
+        contactList = (ListView) findViewById(R.id.contactList);
+
+        // Defined Array values to show in ListView
+        String[] contacts = new String[] { "Thuan Trinh",
+                "Michael Siruno",
+                "Shane Clays"
+        };
+
+        // Define a new Adapter
+        // First parameter - Context
+        // Second parameter - Layout for the row
+        // Third parameter - ID of the TextView to which the data is written
+        // Forth - the Array of data
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, contacts);
+
+        // Assign adapter to ListView
+        contactList.setAdapter(adapter);
+
+        // ListView Item Click Listener
+        contactList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
             @Override
-            public void onClick(View v) {
-                finish();
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+                // ListView Clicked item index
+                int itemPosition     = position;
+
+                // ListView Clicked item value
+                String  itemValue    = (String) contactList.getItemAtPosition(position);
+
+                // Show Alert
+                Toast.makeText(getApplicationContext(),
+                        "Position :"+itemPosition+"  ListItem : " +itemValue , Toast.LENGTH_LONG)
+                        .show();
             }
         });
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
